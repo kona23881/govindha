da661d67f75a8c75e17b754e78ca03778eee5c6e
kona reddy <kona23881@gmail.com>
2025-04-18 19:34:22 +0530

    intial commit in the repository

---
+++ script.js
@@ -0,0 +1,7 @@
+// alert('hi');
+
+let a = document.createElement('h1'); // Create an <h1> element
+a.style.textAlign = 'center';
+a.innerHTML = 'This is webpage';      // Set its inner content
+
+document.body.appendChild(a);         // Append it to the body of the document
